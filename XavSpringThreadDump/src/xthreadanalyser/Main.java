763914640c5a479ae9fa7ff7833e23c8235aec38
merlx <merlx@lmerlxw7a.rpega.com>
2018-09-11 01:04:54 +0100

    new commit

---
+++ XavSpringThreadDump/src/xthreadanalyser/Main.java
@@ -0,0 +1,36 @@
+package xthreadanalyser;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			//View xavview= new View();
+
+			//BorderPane root= xavview.load()
+			primaryStage.setTitle("Utsor 5.0bc");
+			final Scene scene = new Scene(new Controller(primaryStage).getView(), 1400, 800, Color.WHITE);
+			//BorderPane root = new BorderPane();
+			//Scene scene = new Scene(root,400,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
